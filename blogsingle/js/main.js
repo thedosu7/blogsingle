86a971be0ca81f72811b93fddd8bc7747a3ce398
Pham Duc <thedosu2001@gmail.com>
2023-10-02 06:16:20 +0700

    Section 1

--- blogsingle/js/main.js
+++ blogsingle/js/main.js
@@ -11,6 +11,17 @@
       });
     }, 1500);
   };
+  var addActive = function () {
+    var header = document.getElementById('menu');
+    var navlinks = header.getElementsByClassName('nav-link');
+    for (var i = 0; i < navlinks.length; i++) {
+      navlinks[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active', '');
+        this.className += ' active';
+      });
+    }
+  };
   var headerFixed = function () {
     if ($('header').hasClass('header-fixed')) {
       var nav = $('#header_main');
@@ -55,5 +66,34 @@
   $(function () {
     headerFixed();
     preloader();
+    addActive();
+  });
+  $(document).ready(function () {
+    $('.video-play').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false,
+      iframe: {
+        patterns: {
+          youtube: {
+            index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+            id: 'v=', // String that splits URL in a two parts, second part should be %id%
+            // Or null - full URL will be returned
+            // Or a function that should return %id%, for example:
+            // id: function(url) { return 'parsed id'; }
+
+            src: 'https://www.youtube.com/embed/Y4UwssndcPA', // URL that will be set as a source for iframe.
+          },
+
+          // you may add here more sources
+        },
+
+        srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+      },
+    });
   });
 })(jQuery);
